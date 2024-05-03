88834dde77f19f591e42bb2c632162c9e6aa99e4
guo <3227942787@qq.com>
2024-05-04 00:12:28 +0800

    更改导航模式

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory,createWebHashHistory } from 'vue-router'
 const routes = [
     {
         name: '图表测试',
@@ -43,7 +43,7 @@ const routes = [
   // 暂时保持简单
 const router = createRouter({
     // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes:routes,
 })
 export default router
